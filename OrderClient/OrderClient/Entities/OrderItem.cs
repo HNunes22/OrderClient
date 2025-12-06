246fd7c5fcf94c4ef22b3c41c2957cd1c56c00cc
Hugo Nunes <hugoanunes2003@gmail.com>
2025-12-06 12:17:39 -0300

    Enum e Classes adicionadas

---
+++ OrderClient/OrderClient/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderClient.Entities
+{
+    internal class OrderItem
+    {
+        public int QuantityItem {  get; set; }
+        public double PriceItem { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem() { }
+
+        public OrderItem(int quantityItem, double priceItem, Product product)
+        {
+            QuantityItem = quantityItem;
+            PriceItem = priceItem;
+            Product = product;
+        }
+    }
+}
